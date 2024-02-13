30817f6b509a537f2e4aef3b784e32cc55f3d235
şöhrett <sohretturaman@gmail.com>
2024-02-13 19:00:46 +0300

    single page app diagram  has been creted

--- part0/part0-5/src/App.js
+++ part0/part0-5/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import data from "./data.json";
 
 function App() {
+  console.log("data", data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> part0 - 5 Single page app diagram </h1>
+      {data.map((note) => {
+        return (
+          <ul style={{ display: "flex", justifyContent: "flex-start" }}>
+            <li>{note.content}</li>
+          </ul>
+        );
+      })}
+
+      <form>
+        <label>
+          Note:
+          <input type="text" name="name" />
+        </label>
+        <button type="post"> Add</button>
+      </form>
     </div>
   );
 }
