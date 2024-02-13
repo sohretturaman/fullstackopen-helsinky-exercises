2f41f6579c7563085c4d41e53529045009140735
şöhrett <sohretturaman@gmail.com>
2024-02-13 18:52:48 +0300

    project has been created for  part0-5 = Single page app diagram

--- part0/part0-4/src/App.js
+++ part0/part0-4/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+/** @format */
+import { useEffect } from "react";
+import data from "./data.json";
 function App() {
+  useEffect(() => {
+    console.log("data", data[0]);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <h1>New note diagram</h1>
+      <div>
+        sequenceDiagram participant browser participant server
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {" "}
+          browser-{">>"}server: GET
+          https://studies.cs.helsinki.fi/exampleapp/notes activate server
+          server--{">>"}browser: HTML document deactivate server
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          browser-{">>"}server: GET
+          https://studies.cs.helsinki.fi/exampleapp/main.css activate server
+          server--{">>"}browser: the css file deactivate server browser-{">>"}
+          server: GET https://studies.cs.helsinki.fi/exampleapp/main.js activate
+          server
+        </p>
+        <p>
+          server--{">>"}browser: the JavaScript file deactivate server Note
+          right of browser: The browser starts executing the JavaScript code
+          that fetches the JSON from the server
+        </p>
+        <p>
+          browser-{">>"}server: GET
+          https://studies.cs.helsinki.fi/exampleapp/data.json activate server
+        </p>
+        <p>
+          server--{">>"}browser: [
+          {("content", "HTML is easy", "date", "2023-1-1")}, ... ] deactivate
+          server Note right of browser: The browser executes the callback
+          function that renders the notes
+        </p>
+      </div>
+      <form></form>
     </div>
   );
 }
