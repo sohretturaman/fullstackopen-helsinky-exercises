c7de502ea7830f111c0880daa981ccab07038cb8
şöhrett <sohretturaman@gmail.com>
2024-02-16 11:06:06 +0300

    exercise 1.2 has been completed in part1

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -1,16 +1,14 @@
 import React from "react";
+import Part from "./Part";
 
 const Content = (props) => {
   return <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+ 
+    <Part part ={props.part1} exercise={props.exercise1} />
+    <Part part = {props.part2} exercise= {props.exercises2} />
+    <Part part ={props.part3} exercise={props.exercise3} />
+   
+      
   </div>;
 };
 
