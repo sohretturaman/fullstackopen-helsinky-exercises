3ce6d3eec2b90301ee81211dca310c1a4472ccbd
şöhrett <sohretturaman@gmail.com>
2024-02-16 12:56:54 +0300

    exercise 1.11 has been completed in part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,58 +1,75 @@
-import { useState } from "react"
+/** @format */
+
+import { useState } from "react";
 import StatisticLine from "./StatisticLine";
 
-
-
-const Statistics =({good,bad,neutral,avarege,getPositive})=>{
-  return <div>
-       <h1>statistics</h1>
-       <StatisticLine text='good' value={good} />
-       <StatisticLine text='neutral' value={neutral} />
-       <StatisticLine text='bad' value={bad} />
-       <StatisticLine text='all' value={bad+good+neutral}/>
-       <StatisticLine text='avarege' value={avarege(good,neutral,bad)} />
-       <StatisticLine text='positive' value={getPositive(good,neutral,bad)} />
-   
-  </div>;
-}
+const Statistics = ({ good, bad, neutral, avarege, getPositive }) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      <tr>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={bad + good + neutral} />
+        <StatisticLine text="avarege" value={avarege(good, neutral, bad)} />
+        <StatisticLine
+          text="positive"
+          value={getPositive(good, neutral, bad)}
+        />
+      </tr>
+    </>
+  );
+};
 
 const App = () => {
- // save clicks of each button to its own state
- const [good, setGood] = useState(0)
- const [neutral, setNeutral] = useState(0)
- const [bad, setBad] = useState(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
- const avarege = (good,neutral,bad)=>{
-  let avrg = (good+neutral+bad)/3; 
-  return avrg; 
- }
- const getPositive =(good,neutral,bad)=>{
-  let total = good+neutral+bad;
-  return good / total +'%'; 
- }
- return (
-   <div>
-     code here
-     <h1> Give Feedback</h1>
-     <button style={{marginRight:5}} onClick={()=>setGood(prev=>prev+1)}>good</button>
-     <button style={{marginRight:5}} onClick={()=>setNeutral((prev)=>prev+1)}>neutral</button>
-     <button onClick={()=>setBad((prev)=>prev+1)} >bad</button>
-{
-bad !==0 || good !==0 || neutral !==0 ? 
-<Statistics good={good} bad={bad} neutral={neutral} getPositive={getPositive} avarege={avarege} />
-:
-<p>No feedback given</p>
-}
-    
-   </div>
- )
-  
-}
+  const avarege = (good, neutral, bad) => {
+    let avrg = (good + neutral + bad) / 3;
+    return avrg;
+  };
+  const getPositive = (good, neutral, bad) => {
+    let total = good + neutral + bad;
+    return good / total + "%";
+  };
+  return (
+    <div>
+      code here
+      <h1> Give Feedback</h1>
+      <button
+        style={{ marginRight: 5 }}
+        onClick={() => setGood((prev) => prev + 1)}
+      >
+        good
+      </button>
+      <button
+        style={{ marginRight: 5 }}
+        onClick={() => setNeutral((prev) => prev + 1)}
+      >
+        neutral
+      </button>
+      <button onClick={() => setBad((prev) => prev + 1)}>bad</button>
+      {bad !== 0 || good !== 0 || neutral !== 0 ? (
+        <Statistics
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          getPositive={getPositive}
+          avarege={avarege}
+        />
+      ) : (
+        <p>No feedback given</p>
+      )}
+      <div></div>
+    </div>
+  );
+};
 
-export default App
-
-
-
+export default App;
 
 /**previous tasks 1-5
  * 
