0a40733f27711a8c2994c608801a8010fbbdbf8b
şöhrett <sohretturaman@gmail.com>
2024-02-17 19:32:25 +0300

    exercise 2.2 has been completed

--- part2/src/Course.jsx
+++ part2/src/Course.jsx
@@ -4,18 +4,20 @@ import React from "react";
 
 const Course = (props) => {
   console.log(props.course);
-
+  let sum = 0;
   return (
     <div>
-      {props.course.map((item, index) => {
+      {props.course.map((item) => {
+        sum = sum + item.exercises;
         return (
-          <div key={index}>
+          <div key={item.id}>
             <p>
               {item.name} {item.exercises}{" "}
             </p>
           </div>
         );
       })}
+      <p style={{ fontWeight: "bold" }}>Total of {sum} exercises </p>
     </div>
   );
 };
