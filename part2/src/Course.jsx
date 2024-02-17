c7b24c3f643c732494a647c9da26b838bd276b07
şöhrett <sohretturaman@gmail.com>
2024-02-17 19:49:59 +0300

    exercise 2.4 has been completed

--- part2/src/Course.jsx
+++ part2/src/Course.jsx
@@ -1,20 +1,24 @@
 /** @format */
 
 import React from "react";
+import Header from "./Header";
 
 const Course = (props) => {
-  const sumOfExercises = props.course.reduce(
+  console.log("props.courses", props.courses);
+
+  const sumOfExercises = props.courses[0].parts.reduce(
     (accumulator, currentVal) => accumulator + currentVal.exercises,
     0
   ); //usign reduce funciton to sum exercises
-  console.log("sum of exercises", sumOfExercises);
+  const sumOfExercises2 = props.courses[1].parts.reduce(
+    (accumulator, currentVal) => accumulator + currentVal.exercises,
+    0
+  );
 
-  console.log(props.course);
-  let sum = 0;
   return (
     <div>
-      {props.course.map((item) => {
-        sum = sum + item.exercises;
+      <Header title={props.courses[0].name} />
+      {props.courses[0].parts.map((item) => {
         return (
           <div key={item.id}>
             <p>
@@ -24,6 +28,22 @@ const Course = (props) => {
         );
       })}
       <p style={{ fontWeight: "bold" }}>Total of {sumOfExercises} exercises </p>
+
+      <div>
+        <Header title={props.courses[1].name} />
+        {props.courses[1].parts.map((item) => {
+          return (
+            <div key={item.id}>
+              <p>
+                {item.name} {item.exercises}{" "}
+              </p>
+            </div>
+          );
+        })}
+        <p style={{ fontWeight: "bold" }}>
+          Total of {sumOfExercises2} exercises{" "}
+        </p>
+      </div>
     </div>
   );
 };
