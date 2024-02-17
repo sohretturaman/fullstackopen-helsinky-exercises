5c5f3ad48473ed1c0fb04332442670a8bcfd1207
şöhrett <sohretturaman@gmail.com>
2024-02-17 19:40:17 +0300

    exercise 2.3 has been completed

--- part2/src/Course.jsx
+++ part2/src/Course.jsx
@@ -3,6 +3,12 @@
 import React from "react";
 
 const Course = (props) => {
+  const sumOfExercises = props.course.reduce(
+    (accumulator, currentVal) => accumulator + currentVal.exercises,
+    0
+  ); //usign reduce funciton to sum exercises
+  console.log("sum of exercises", sumOfExercises);
+
   console.log(props.course);
   let sum = 0;
   return (
@@ -17,7 +23,7 @@ const Course = (props) => {
           </div>
         );
       })}
-      <p style={{ fontWeight: "bold" }}>Total of {sum} exercises </p>
+      <p style={{ fontWeight: "bold" }}>Total of {sumOfExercises} exercises </p>
     </div>
   );
 };
