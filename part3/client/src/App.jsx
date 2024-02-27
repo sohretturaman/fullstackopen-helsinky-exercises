d2a55e6ff2af6e4e2fd42795dbdb3415bfb4b010
şöhrett <sohretturaman@gmail.com>
2024-02-27 20:01:14 +0300

    completed frontend configuration

--- part3/client/src/App.jsx
+++ part3/client/src/App.jsx
@@ -1,18 +1,23 @@
 /** @format */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Persons from "./Persons";
 
 //const baseUrl = "http://localhost:3001/api/persons";
 
 const App = () => {
+  const [data, setData] = useState({});
   const getAll = async () => {
-    /*   const response = axios.get("http://localhost:3001/api/persons");
-    console.log("response", response); */
+    const response = await axios.get("http://localhost:3001/api/persons");
+    setData(response.data.data);
 
-    await fetch("http://localhost:3001/api/persons")
+    /*     await fetch("http://localhost:3001/api/persons")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setData(data.data);
+        console.log("data", data.data);
+      }); */
   };
 
   useEffect(() => {
@@ -22,6 +27,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      {data.length > 0 && <Persons persons={data} />}
     </div>
   );
 };
