aa4c982349fe411efbb1f29c74c92b1f99efc8ba
şöhrett <sohretturaman@gmail.com>
2024-03-01 10:42:23 +0300

    exercise 4.4 has been completed

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,6 +4,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (array) => {
+  let sum = 0;
+  const result = array.forEach((num) => {
+    sum += num;
+  });
+  return array.length === 0 ? 0 : result;
+};
+
 module.exports = {
+  totalLikes,
   dummy,
 };
