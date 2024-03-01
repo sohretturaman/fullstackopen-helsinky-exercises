e8f28b6b0208f624207c6743da8ba70a75a8e31b
şöhrett <sohretturaman@gmail.com>
2024-03-01 11:02:47 +0300

    exercises 4.5,4.6 and 4.7 have been completed

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,12 +1,13 @@
 /** @format */
 
+// list_helper.test.js
+
 const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
 test("dummy returns one", () => {
   const blogs = [];
-
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
@@ -26,5 +27,65 @@ describe("total likes", () => {
   test("when list has only one blog, equals the likes of that", () => {
     const result = listHelper.totalLikes(listWithOneBlog);
     assert.strictEqual(result, 5);
+  });
+});
+
+describe("favorite blog", () => {
+  const blogs = [
+    {
+      title: "Blog 1",
+      author: "Author 1",
+      likes: 10,
+    },
+    {
+      title: "Blog 2",
+      author: "Author 2",
+      likes: 20,
+    },
+    {
+      title: "Blog 3",
+      author: "Author 3",
+      likes: 15,
+    },
+  ];
+
+  test("returns the blog with the most likes", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, {
+      title: "Blog 2",
+      author: "Author 2",
+      likes: 20,
+    });
+  });
+});
+
+describe("most blogs", () => {
+  const blogs = [
+    { author: "Author 1" },
+    { author: "Author 2" },
+    { author: "Author 2" },
+    { author: "Author 3" },
+    { author: "Author 3" },
+    { author: "Author 3" },
+  ];
+
+  test("returns the author with the most blogs", () => {
+    const result = listHelper.mostBlogs(blogs);
+    assert.deepStrictEqual(result, { author: "Author 3", blogs: 3 });
+  });
+});
+
+describe("most likes", () => {
+  const blogs = [
+    { author: "Author 1", likes: 10 },
+    { author: "Author 2", likes: 20 },
+    { author: "Author 2", likes: 15 },
+    { author: "Author 3", likes: 25 },
+    { author: "Author 3", likes: 30 },
+  ];
+
+  test("returns the author with the most likes", () => {
+    const result = listHelper.mostLikes(blogs);
+    assert.deepStrictEqual(result, { author: "Author 3", likes: 55 });
   });
 });
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,18 +1,80 @@
 /** @format */
 
+// list_helper.js
+
 const dummy = (blogs) => {
   return 1;
 };
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0);
+};
 
-const totalLikes = (array) => {
-  let sum = 0;
-  const result = array.forEach((num) => {
-    sum += num;
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  let maxLikes = 0;
+  let favorite = null;
+
+  blogs.forEach((blog) => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes;
+      favorite = blog;
+    }
   });
-  return array.length === 0 ? 0 : result;
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const blogCounts = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + 1;
+    return acc;
+  }, {});
+
+  const authorWithMostBlogs = Object.keys(blogCounts).reduce((a, b) => {
+    return blogCounts[a] > blogCounts[b] ? a : b;
+  });
+
+  return {
+    author: authorWithMostBlogs,
+    blogs: blogCounts[authorWithMostBlogs],
+  };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const likesByAuthor = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + blog.likes;
+    return acc;
+  }, {});
+
+  const authorWithMostLikes = Object.keys(likesByAuthor).reduce((a, b) => {
+    return likesByAuthor[a] > likesByAuthor[b] ? a : b;
+  });
+
+  return {
+    author: authorWithMostLikes,
+    likes: likesByAuthor[authorWithMostLikes],
+  };
 };
 
 module.exports = {
-  totalLikes,
   dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
